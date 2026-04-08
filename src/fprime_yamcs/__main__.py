fcedc1fee1527cc1e3a5bcab361c1abf33a4ec03
M Starch <LeStarch@googlemail.com>
2026-04-07 18:16:30 -0700

    Building config from CLI and fprime-dictionary

--- src/fprime_yamcs/__main__.py
+++ src/fprime_yamcs/__main__.py
@@ -19,10 +19,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import atexit
 import os
+import json
 import shutil
 import subprocess
 import sys
+import tempfile
 
 import yaml
 
@@ -41,12 +44,6 @@ class YamcsParser(ParserBase):
     def get_arguments(self) -> Dict[Tuple[str, ...], Dict[str, Any]]:
         """Arguments to handle deployments"""
         return {
-            ("--no-convert-dictionary",): {
-                "dest": "convert_dictionary",
-                "action": "store_false",
-                "default": True,
-                "help": "Convert the F Prime dictionary to XTCE/YAMCS format. Default: %(default)s",
-            },
             ("--yamcs-config-dir",): {
                 "action": "store",
                 "default": files("fprime_yamcs").joinpath("yamcs/src/main/yamcs"),
@@ -65,6 +62,18 @@ class YamcsParser(ParserBase):
                 "type": Path,
                 "help": "Specify the YAMCS instance to use for fprime-events",
             },
+            ("--udp-uplink-port", ): {
+                "action": "store",
+                "default": 50001,
+                "type": int,
+                "help": "Specify the UDP port for uplink (TC) communication with YAMCS. Default: %(default)s",
+            },
+            ("--udp-downlink-port", ): {
+                "action": "store",
+                "default": 50000,
+                "type": int,
+                "help": "Specify the UDP port for downlink (TM) communication with YAMCS. Default: %(default)s",
+            },
         }
 
     def handle_arguments(self, args, **kwargs):
@@ -77,7 +86,7 @@ class YamcsParser(ParserBase):
 
 def yamcs_instances(config_directory: Path) -> List[str]:
     """ Load the YAMCS instance names from the configuration directory
-    
+
     This reads instance configurations from "etc/yamcs.yml" under the supplied configuration directory and extracts the
     instance names from the instance list.
 
@@ -100,9 +109,9 @@ def yamcs_instances(config_directory: Path) -> List[str]:
         raise Exception(f"No instances found in YAMCS configuration {yamcs_yaml}")
 
 
-def xtce_mdb_location(config_directory: Path, instances: List[str]) -> Path:
+def xtce_mdb_location(config_directory: Path, instances: List[str]) -> Tuple[Path, str]:
     """ Load the YAMCS XTCE MDB location from the instance configuration
-    
+
     YAMCS allows multiple instances with multiple MDBs. This function looks for the first MDB instance of type "xtce"
     and a file argument that ends with "fprime.xtce.xml". This is the XTCE XML that should be updated from the F Prime
     dictionary.
@@ -113,7 +122,7 @@ def xtce_mdb_location(config_directory: Path, instances: List[str]) -> Path:
         config_directory: The YAMCS configuration directory to search for instance configurations
         instances: A list of YAMCS instance names to consider
     Returns:
-        The path to the XTCE MDB file to update
+        The path to the XTCE MDB file to update and the instance it was found in
     """
     # Read the instance file to find the MDB location
     for instance in instances:
@@ -139,6 +148,82 @@ def xtce_mdb_location(config_directory: Path, instances: List[str]) -> Path:
             return config_directory / file_path, instance
     else:
         raise Exception(f"No valid YAMCS instance found in {config_directory / 'etc'}")
+
+def get_dictionary_constants(dictionary: Path, constants: List[str]) -> str:
+    """ Get the dictionary constant from the F Prime dictionary path
+
+    This extracts constants from the F Prime dictionary.
+
+    Args:
+        dictionary: The path to the F Prime dictionary file
+        constants: A list of constant names to look for in the dictionary
+    Returns:
+        a list of constants found in the dictionary that match the supplied list of constant names
+    """
+    with dictionary.open() as f:
+        dictionary_data = json.load(f)
+        constants_data = dictionary_data.get("constants", [])
+    found_constants = [
+        constant["value"] for constant in constants_data if constant.get("qualifiedName", "") in constants
+    ]
+    if len(found_constants) != len(constants):
+        raise ValueError(f"Required constants {constants} not found in dictionary")
+    return found_constants
+
+
+def construct_temporary_configuration(config_directory: Path, instances: List[str], dictionary: Path, uplink_port: int, downlink_port: int) -> Tuple[Path, str]:
+    """ Construct a temporary YAMCS configuration directory
+
+    The YAMCS configuration that ships with fprime-yamcs needs to be modified in several specific ways before running
+    with YAMCS. These include:
+        1. Updating the XTCE MDB file with the converted F Prime dictionary
+        2. Updating the TM/TC processors to use the correct UDP ports
+        3. Updating the TM/TC processors to use the correct dictionary constants
+    Args:
+        config_directory: The YAMCS configuration directory to use as a base for the temporary configuration
+        instances: A list of YAMCS instance names to consider for configuration
+        dictionary: The path to the F Prime dictionary file to convert and use for the XTCE MDB
+        uplink_port: The UDP port to use for uplink (TC) communication with YAMCS
+        downlink_port: The UDP port to use for downlink (TM) communication with YAMCS
+    Returns:
+        The path to the temporary YAMCS configuration directory and the fprime identified instance
+    """
+
+    # Create a temporary configuration directory that will be destroyed on exit
+    yamcs_working_config_dir = Path(tempfile.mkdtemp())
+    atexit.register(lambda: shutil.rmtree(yamcs_working_config_dir))
+
+    # Copy the default configuration to the temporary directory
+    shutil.copytree(config_directory, yamcs_working_config_dir, dirs_exist_ok=True)
+    xtce_dictionary, fprime_instance = xtce_mdb_location(yamcs_working_config_dir, instances)
+
+    print(f"[INFO] Updating YAMCS XTCE dictionary from {dictionary} to {xtce_dictionary}")
+    subprocess.run(["fprime-to-xtce", "-o", str(xtce_dictionary), str(dictionary)], check=True)
+
+    print("[INFO] Setting ports for YAMCS UDP processors")
+    instance_path = yamcs_working_config_dir / "etc" / f"yamcs.{fprime_instance}.yaml"
+    assert instance_path.is_file(), f"YAMCS instance configuration {instance_path} not found."
+    with instance_path.open() as f:
+        instance_config = yaml.safe_load(f)
+    constants = get_dictionary_constants(dictionary, ["ComCfg.TmFrameFixedSize", "ComCfg.SpacecraftId"])
+    for link in instance_config.get("dataLinks", []):
+        print(link)
+        if link.get("class", "") == "org.yamcs.tctm.ccsds.UdpTmFrameLink":
+            print(f"[INFO] Setting downlink port for TM link {link.get('name', '')} to {downlink_port}")
+            link["port"] = downlink_port
+            link["frameLength"] = constants[0]
+            link["spacecraftId"] = constants[1]
+            for vc in link.get("virtualChannels", []):
+                vc["maxPacketLength"] = constants[0]
+        elif link.get("class", "") == "org.yamcs.tctm.ccsds.UdpTcFrameLink":
+            print(f"[INFO] Setting downlink port for TM link {link.get('name', '')} to {downlink_port}")
+            link["port"] = uplink_port
+            link["maxFrameLength"] = constants[0]
+            link["spacecraftId"] = constants[1]
+    with instance_path.open("w") as f:
+        yaml.safe_dump(instance_config, f)
+
+    return  yamcs_working_config_dir, fprime_instance
 
 
 def launch_yamcs(parsed_args):
@@ -181,11 +266,11 @@ def parse_args():
         argument_handlers, "Run F prime deployment and GDS"
     )
     return args
-    
+
 
 def main():
     """ Main entrypoint for F Prime YAMCS
-    
+
     This performs the argument processing, and then starts F Prime YAMCS.
     """
     parsed_args = parse_args()
@@ -194,16 +279,13 @@ def main():
         instances = yamcs_instances(parsed_args.yamcs_config_dir)
         if not instances:
             raise Exception(f"No YAMCS instances found in {parsed_args.yamcs_config_dir / 'etc/yamcs.yaml'}")
-        events_instance = instances[0]
-        if parsed_args.convert_dictionary:
-            xtce_dictionary, events_instance = xtce_mdb_location(parsed_args.yamcs_config_dir, instances)
-            print(f"[INFO] Updating YAMCS XTCE dictionary from {parsed_args.dictionary} to {xtce_dictionary}")
-            subprocess.run(["fprime-to-xtce", "-o", str(xtce_dictionary), str(parsed_args.dictionary)],
-                           check=True)
+
+        yamcs_config_dir, fprime_instance = construct_temporary_configuration(parsed_args.yamcs_config_dir, instances, parsed_args.dictionary, parsed_args.udp_uplink_port, parsed_args.udp_downlink_port)
+        parsed_args.yamcs_config_dir = yamcs_config_dir
         if parsed_args.yamcs_events_instance is None:
-            parsed_args.yamcs_events_instance = events_instance
-        launch_app = [launch_app] if parsed_args.app is not None else []
-        processes = [launcher(parsed_args) for launcher in launch_app + [launch_yamcs]]
+            parsed_args.yamcs_events_instance = fprime_instance
+        launched_apps = [launch_app] if parsed_args.app is not None else []
+        processes = [launcher(parsed_args) for launcher in launched_apps + [launch_yamcs]]
         print("[INFO] F Prime/YAMCS is now running. CTRL-C to shutdown all components.")
         processes[-1].wait()
     except KeyboardInterrupt:
